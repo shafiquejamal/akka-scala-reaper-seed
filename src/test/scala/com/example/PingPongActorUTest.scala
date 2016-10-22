211b3caaea679cd6ee4d0a792604d174d8f18d8c
Shafique Jamal <shafique.jamal@gmail.com>
2016-10-21 20:54:04 -0400

    Add scoverage. Convert to FlatSPec.

--- src/test/scala/com/example/PingPongActorUTest.scala
+++ src/test/scala/com/example/PingPongActorUTest.scala
@@ -3,10 +3,14 @@ package com.example
 import akka.actor.{Props, ActorSystem}
 import akka.testkit.{ImplicitSender, TestKit}
 import com.example.util.ReaperImpl
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import org.scalatest.{FlatSpecLike, BeforeAndAfterAll, Matchers, WordSpecLike}
  
-class PingPongActorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
-  with WordSpecLike with Matchers with BeforeAndAfterAll {
+class PingPongActorUTest(_system: ActorSystem)
+  extends TestKit(_system)
+  with ImplicitSender
+  with FlatSpecLike
+  with Matchers
+  with BeforeAndAfterAll {
  
   def this() = this(ActorSystem("MySpec"))
  
@@ -18,18 +22,14 @@ class PingPongActorSpec(_system: ActorSystem) extends TestKit(_system) with Impl
   val pongActor = system.actorOf(Props(classOf[PongActor], reaper), "pongActor")
   val pingActor = system.actorOf(Props(classOf[PingActor], reaper, pongActor), "pingActor")
 
-  "A Ping actor" must {
-    "send back a ping on a pong" in {
+  "A Ping actor" should "send back a ping on a pong" in {
       pingActor ! PongActor.PongMessage("pong")
       expectMsg(PingActor.PingMessage("ping"))
-    }
   }
 
-  "A Pong actor" must {
-    "send back a pong on a ping" in {
+  "A Pong actor" should "send back a pong on a ping" in {
       pongActor ! PingActor.PingMessage("ping")
       expectMsg(PongActor.PongMessage("pong"))
-    }
   }
 
 }
